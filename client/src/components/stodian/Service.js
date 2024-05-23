ed94767bdcf76833dd935a21aba117b54ea1fdd0
Ethan Reid <163367994+Stodian@users.noreply.github.com>
2024-05-23 14:16:57 +0100

    typing error fix

--- client/src/components/stodian/Service.js
+++ client/src/components/stodian/Service.js
@@ -1,135 +1,184 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
 
 const Service = () => {
+  const dynamicTextRef = useRef('');
   const [dynamicText, setDynamicText] = useState('');
-  const dynamicRef = useRef(null);  
+  const dynamicRef = useRef(null);
+  const button1Ref = useRef(null);
+  const button2Ref = useRef(null);
+  const currentMessageIndex = useRef(0);
+  const charIndex = useRef(0);
+  const timeoutId = useRef(null);
 
   useEffect(() => {
     const messages = ["Residential", "Commercial", "Industrial", "Educational", "Public"];
     const typingSpeed = 150;
     const deletingSpeed = 75;
-    let currentMessage = 0;
-    let charIndex = 0;
-    let timeoutId; // Store the timeout ID for cleanup
 
     function typeLetter() {
-      if (charIndex < messages[currentMessage].length) {
-        setDynamicText(prevText => prevText + messages[currentMessage].charAt(charIndex));
-        charIndex++;
-        timeoutId = setTimeout(typeLetter, typingSpeed); // Store timeout ID
+      if (charIndex.current < messages[currentMessageIndex.current].length) {
+        dynamicTextRef.current += messages[currentMessageIndex.current].charAt(charIndex.current);
+        setDynamicText(dynamicTextRef.current);  // Trigger re-render
+        charIndex.current++;
+        timeoutId.current = setTimeout(typeLetter, typingSpeed);
       } else {
-        timeoutId = setTimeout(deleteText, 2000); // Store timeout ID
+        timeoutId.current = setTimeout(deleteText, 2000);
       }
     }
 
     function deleteText() {
-      if (charIndex > 0) {
-        setDynamicText(prevText => prevText.substring(0, charIndex - 1));
-        charIndex--;
-        timeoutId = setTimeout(deleteText, deletingSpeed); // Store timeout ID
+      if (charIndex.current > 0) {
+        dynamicTextRef.current = dynamicTextRef.current.slice(0, -1);
+        setDynamicText(dynamicTextRef.current);  // Trigger re-render
+        charIndex.current--;
+        timeoutId.current = setTimeout(deleteText, deletingSpeed);
       } else {
-        currentMessage = (currentMessage + 1) % messages.length;
-        timeoutId = setTimeout(typeLetter, 500); // Store timeout ID
+        currentMessageIndex.current = (currentMessageIndex.current + 1) % messages.length;
+        timeoutId.current = setTimeout(typeLetter, 500);
       }
     }
 
     const span = dynamicRef.current;
-    if (span) {  // Ensure the span is mounted before starting
+    if (span) {
       typeLetter();
     }
 
     return () => {
-      clearTimeout(timeoutId); // Clear any pending timeouts on unmount
+      clearTimeout(timeoutId.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleHover = (element, scale) => {
+      gsap.to(element, { scale: scale, duration: 0.3, boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' });
+    };
+
+    const handleHoverOut = (element) => {
+      gsap.to(element, { scale: 1, duration: 0.3, boxShadow: 'none' });
+    };
+
+    const button1 = button1Ref.current;
+    const button2 = button2Ref.current;
+
+    if (button1) {
+      button1.addEventListener('mouseenter', () => handleHover(button1, 1.05));
+      button1.addEventListener('mouseleave', () => handleHoverOut(button1));
+    }
+
+    if (button2) {
+      button2.addEventListener('mouseenter', () => handleHover(button2, 1.05));
+      button2.addEventListener('mouseleave', () => handleHoverOut(button2));
+    }
+
+    return () => {
+      if (button1) {
+        button1.removeEventListener('mouseenter', () => handleHover(button1, 1.05));
+        button1.removeEventListener('mouseleave', () => handleHoverOut(button1));
+      }
+
+      if (button2) {
+        button2.removeEventListener('mouseenter', () => handleHover(button2, 1.05));
+        button2.removeEventListener('mouseleave', () => handleHoverOut(button2));
+      }
     };
   }, []);
 
   const pageStyle = {
-    height: '100vh',  // 100% of the viewport height
+    height: '100vh',
     display: 'flex',
     flexDirection: 'column',
     marginTop: '200px',
     alignItems: 'center',
-    fontSize: '3.5rem',               /* Large text similar to h1 */
-    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif", /* Font styling */
-    backgroundColor: '#transparent',       /* Light gray background color */
-    color: '#333',                     /* Dark gray text color */
-    fontWeight: 'bold', /* Makes the text bold */
+    fontSize: '3.5rem',
+    fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+    backgroundColor: 'transparent',
+    color: '#333',
+    fontWeight: 'bold',
     zIndex: 2,
   };
 
-
   return (
-<main id="page1" className="page" style={pageStyle}>
-  <div className="typed-words">
-    <span id="constant1">Your Partner in </span>
-    <span id="dynamic" ref={dynamicRef}>
-      {dynamicText}
-      <span className="blinking-cursor">|</span> 
-    </span>
-    <span id="constant2"> Design</span>
-  </div>
+    <main id="page1" className="page" style={pageStyle}>
+      <div className="typed-words">
+        <span id="constant1">Your Partner in </span>
+        <span id="dynamic" ref={dynamicRef}>
+          {dynamicText}
+          <span className="blinking-cursor">|</span>
+        </span>
+        <span id="constant2"> Design</span>
+      </div>
 
-  <style jsx>{`
-    .blinking-cursor { /* Target the blinking-cursor class directly */
-      animation: blink-caret 1s step-end infinite;
-    }
+      <style jsx>{`
+        .blinking-cursor {
+          animation: blink-caret 1s step-end infinite;
+        }
 
-    @keyframes blink-caret {
-      from, to {
-        opacity: 0; 
-      }
-      50% {
-        opacity: 1;
-      }
-    }
-  `}</style>
-      
-      <div style={{ display: 'flex' }}> {/* Container to hold the buttons */}
-      
-             <button className="button1" style={{
-                backgroundColor: '#8B0000', // Maroon color
-                color: 'white',
-                padding: '12px 24px',
-                border: 'none',
-                borderRadius: '5px',
-                fontSize: '18px',
-                cursor: 'pointer',
-                margin: '10px', // Add some spacing between buttons
-                transition: 'all 0.3s ease', /* Smooth transition for hover effects */
-                fontFamily: "'Arial', sans-serif", /* Clean and professional font */
-                fontWeight: 'bold', /* Makes text bold to stand out */
-                textTransform: 'uppercase', /* Uppercase text for a more impactful appearance */
-                outline: 'none', /* Removes the outline to clean up focus style */
-                zIndex: 1, /* Keeps button above other content */
-            }}>
+        @keyframes blink-caret {
+          from, to {
+            opacity: 0;
+          }
+          50% {
+            opacity: 1;
+          }
+        }
 
-               Free Custom Design
+        .button1, .button2 {
+          transition: all 0.3s ease;
+        }
 
-             </button>
+        .button1:hover {
+          background-color: #a00000; /* Darker maroon color */
+        }
 
-             <button className="button2" style={{
-                backgroundColor: 'white', // Maroon color
-                color: '#800000',
-                padding: '12px 24px',
-                border: '2px solid #ccc', /* Soft grey border for subtle definition */
-                borderRadius: '5px',
-                fontSize: '18px',
-                cursor: 'pointer',
-                margin: '10px', // Add some spacing between buttons
-                transition: 'all 0.3s ease', /* Smooth transition for hover effects */
-                fontFamily: "'Arial', sans-serif", /* Clean and professional font */
-                fontWeight: 'bold', /* Makes text bold to stand out */
-                textTransform: 'uppercase', /* Uppercase text for a more impactful appearance */
-                outline: 'none', /* Removes the outline to clean up focus style */
-                zIndex: 1, /* Keeps button above other content */
-            }}>
-              How it Works
-            </button>
-        </div>
-    
+        .button2:hover {
+          background-color: #f0f0f0; /* Light gray color */
+          color: #a00000; /* Darker maroon color */
+        }
+      `}</style>
 
+      <div style={{ display: 'flex' }}>
+        <button ref={button1Ref} className="button1" style={buttonStyle1}>
+          Free Custom Design
+        </button>
+        <button ref={button2Ref} className="button2" style={buttonStyle2}>
+          How it Works
+        </button>
+      </div>
     </main>
   );
+};
+
+const buttonStyle1 = {
+  backgroundColor: '#8B0000',
+  color: 'white',
+  padding: '12px 24px',
+  border: 'none',
+  borderRadius: '5px',
+  fontSize: '18px',
+  cursor: 'pointer',
+  margin: '10px',
+  fontFamily: "'Arial', sans-serif",
+  fontWeight: 'bold',
+  textTransform: 'uppercase',
+  outline: 'none',
+  zIndex: 1,
+};
+
+const buttonStyle2 = {
+  backgroundColor: 'white',
+  color: '#800000',
+  padding: '12px 24px',
+  border: '2px solid #ccc',
+  borderRadius: '5px',
+  fontSize: '18px',
+  cursor: 'pointer',
+  margin: '10px',
+  fontFamily: "'Arial', sans-serif",
+  fontWeight: 'bold',
+  textTransform: 'uppercase',
+  outline: 'none',
+  zIndex: 1,
 };
 
 export default Service;
